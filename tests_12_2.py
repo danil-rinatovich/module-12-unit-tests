1267045687f209a0951ff1d7208cad8002d57c63
danil_rinatovich <danil_rinatovich@list.ru>
2025-01-15 01:56:37 +0500

    finished

--- tests_12_2.py
+++ tests_12_2.py
@@ -1,7 +1,4 @@
 import unittest
-from pprint import pprint
-from unittest import TestCase
-
 
 class Runner:
     def __init__(self, name, speed=5):
@@ -16,6 +13,9 @@ class Runner:
         self.distance += self.speed
 
     def __str__(self):
+        return self.name
+
+    def __repr__(self):
         return self.name
 
     def __eq__(self, other):
@@ -45,7 +45,7 @@ class Tournament:
 
 
 class TournamentTest(unittest.TestCase):
-
+    @classmethod
     def setUpClass(self):
         self.all_results = {}
 
@@ -54,24 +54,28 @@ class TournamentTest(unittest.TestCase):
         self.andrey_runner = Runner('Андрей', 9)
         self.nick_runner = Runner('Ник', 3)
 
+    @classmethod
     def tearDownClass(self):
         for k, v in self.all_results.items():
             print(f'{k}: {v}')
 
     def test_usain_and_nick(self):
-        tournament = Tournament(90, [self.usain_runner, self.nick_runner])
-        tournament.start()
-        self.assertTrue(tournament.participants[-1].name == "Ник")
+        tournament = Tournament(90, self.usain_runner, self.nick_runner)
+        result = tournament.start()
+        self.all_results[1] = result
+        self.assertTrue(result[max(result.keys())].name, self.nick_runner)
 
     def test_andrey_and_nick(self):
-        tournament = Tournament(90, [self.andrey_runner, self.nick_runner])
-        tournament.start()
-        self.assertTrue(tournament.participants[-1].name == "Ник")
+        tournament = Tournament(90, self.andrey_runner, self.nick_runner)
+        result = tournament.start()
+        self.all_results[2] = result
+        self.assertTrue(result[max(result.keys())].name, self.nick_runner)
 
     def test_usain_andrey_and_nick(self):
-        tournament = Tournament(90, [self.usain_runner, self.andrey_runner, self.nick_runner])
-        tournament.start()
-        self.assertTrue(tournament.participants[-1].name == "Ник")
+        tournament = Tournament(90, self.usain_runner, self.andrey_runner, self.nick_runner)
+        result = tournament.start()
+        self.all_results[3] = result
+        self.assertTrue(result[max(result.keys())].name, self.nick_runner)
 
 
 if __name__ == '__main__':
